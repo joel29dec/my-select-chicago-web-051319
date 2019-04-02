75e5a4e8d0497cecb7e9bbc86e84eece0fd15937
Joel S <joel29dec@gmail.com>
2019-04-02 01:11:24 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -3,12 +3,17 @@ def my_select(array)
     i = 0
     new_arr = [] 
      while i < array.length
-      new_arr << yield(array[i])
+      if yield(array[i]) == true
+        
+      end
       i = i + 1
     end
     new_arr
   end
 end
 
-
+my_select(nums) do |num|
+      num.even?
+      end
+    
 #The .select method returns a new collection containing all of the elements in the submitted collection for which the block's conditional is true
