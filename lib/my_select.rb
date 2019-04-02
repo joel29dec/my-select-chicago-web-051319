573877e8cb56bca5a5e177459427d4c91a83fa73
Joel S <joel29dec@gmail.com>
2019-04-02 01:06:27 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,13 @@
-def my_select(collection)
- # your code here!
+def my_select(array)
+  if block_given?
+    i = 0
+    new_arr = [] 
+     while i < array.length
+      new_arr << yield(array[i])
+      i = i + 1
+    end
+    new_arr
+  end
 end
+
+
